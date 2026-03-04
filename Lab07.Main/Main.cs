12e82cbb4a2a13f885d5e13e7add0c18505b538b
JOSnow985 <jaden.olvera@students.snow.edu>
2026-03-03 23:54:42 -0700

    Implemented Main, only placeholder text and a bit rough

--- Lab07.Main/Main.cs
+++ Lab07.Main/Main.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab07;
+
+DoublyLinkedList<string> list = new();
+while (true)
+{
+    Console.Clear();
+    PrintMenu();
+    switch (Console.ReadKey(true).Key)
+    {
+        // Escape ends the program
+        case ConsoleKey.Escape:
+            return;
+        case ConsoleKey.B:
+            Console.WriteLine("Add to beginning");
+            list.AddFirst(CollectString());
+            break;
+        case ConsoleKey.E:
+            Console.WriteLine("Add to end");
+            list.AddLast(CollectString());
+            break;
+        // If the list printing needs to be something the user prompts, use this instead
+        // case ConsoleKey.P:
+        //     if (list.Length is not 0)
+        //     {
+        //         Console.Clear();
+        //         foreach(string s in list)
+        //             Console.WriteLine(s);
+        //         Console.ReadKey(true);
+        //     }
+        //     break;
+        default:
+            break;
+    }
+}
+
+// --- Methods ---
+void PrintMenu()
+{
+    // Print some kind of header
+    Console.WriteLine("Header placeholder");
+    // Print Controls
+    Console.WriteLine("Controls placeholder");
+    // If list is not empty, print the list
+    if (list.Length is not 0)
+        foreach(string s in list)
+            Console.WriteLine(s);
+}
+
+string CollectString()
+{
+    while (true)
+    {
+        string? s = Console.ReadLine();
+        if (s is not null) return s;
+        else
+            Console.WriteLine("null, try again placeholder");
+    }
+}
